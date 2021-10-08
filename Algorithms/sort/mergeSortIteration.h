06aba3dd31b449883be34dad7849a03540916bf7
Jiawei <40544598+Jiawch@users.noreply.github.com>
2021-10-08 23:17:28 +0800

    Update mergeSortIteration.h

--- Algorithms/sort/mergeSortIteration.h
+++ Algorithms/sort/mergeSortIteration.h
@@ -71,3 +71,48 @@ void MergeSortIteration<T>::merge(vector<T> &a, vector<T> &b, int low, int mid, 
 }
 
 #endif
+
+
+
+/*! Author: Jiawch
+ *! Date: 2021-10-08
+ *  迭代实现的归并排序
+ */
+
+
+void mergeSortIteration(vector<int> &a){
+    vector<int> b(a.size());
+    int segment_size = 1;
+    while(segment_size < a.size()){
+        sortIteration(a, b, a.size(), segment_size);
+        segment_size *= 2;
+    }
+}
+void sortIteration(vector<int> &a, vector<int> &b, int n, int segment_size){
+    int i = 0;
+    for (; i + 2*segment_size <= n; i += 2*segment_size){
+        merge(a, b, i, i + segment_size - 1, i + 2*segment_size - 1);
+    }
+    // 少于两段的数据
+    if (i + segment_size < n){
+        merge(a, b, i, i + segment_size - 1, n - 1);  // 剩有两段的数据
+    }
+}
+void merge(vector<int> &a, vector<int> &b, int low, int mid, int high){
+    int i = low;                    // i指向a左边数组的第一个元素
+    int j = mid + 1;                // j指向a右边数组的第一个元素
+    int k = low;                    // k为b中的指针
+    while (i <= mid && j <= high){
+        if (a[i] < a[j])
+            b[k++] = a[i++];
+        else
+            b[k++] = a[j++];
+    }
+    while (i <= mid)
+        b[k++] = a[i++];
+    while (j <= high)
+        b[k++] = a[j++];
+    // 将b[low ... high] 拷贝回 a[low ... high]
+    for (int i = low; i <= high; i++)
+        a[i] = b[i];
+}
