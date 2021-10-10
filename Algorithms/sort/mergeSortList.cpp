f776a95507c52dc8377dba3ceb33facdba2ed103
Jiawei <40544598+Jiawch@users.noreply.github.com>
2021-10-10 19:56:38 +0800

    Update mergeSortList.cpp

--- Algorithms/sort/mergeSortList.cpp
+++ Algorithms/sort/mergeSortList.cpp
@@ -75,3 +75,60 @@ int main() {
     cout << endl;    
 }
 
+
+
+
+/*! Author: Jiawch
+ *! Date: 2021-10-10
+ *! 链表的归并排序
+ *  思路：
+ *    与数组的归并排序类似
+ *  时间复杂度：O(nlogn)
+ *  空间复杂度：O(1)
+ */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(): val(0), next(nullptr) {}
+    ListNode(int val): val(val), next(nullptr) {}
+    ListNode(int val, ListNode *next): val(val), next(next) {}
+};
+
+ListNode* mergeSort(ListNode *head) {
+    if (head == nullptr || head->next == nullptr) return head;
+    ListNode *mid = getMid(head);
+    ListNode *head2 = mid->next;
+    mid->next = nullptr;
+
+    head = mergeSort(head);
+    head2 = mergeSort(head2);
+    return merge(head, head2);
+}
+
+ListNode* merge(ListNode *head1, ListNode *head2) {
+    if (head1 == nullptr) return head2;
+    if (head2 == nullptr) return head1;
+
+    ListNode *head;
+    if (head1->val <= head2->val) {
+        head = head1;
+        head->next = merge(head1->next, head2);
+    } else {
+        head = head2;
+        head->next = merge(head1, head2->next);
+    }
+    return head;
+}
+
+ListNode* getMid(ListNode *head){
+    ListNode *dummp = new ListNode(0, head);
+    ListNode *p_fast = dummp;
+    ListNode *p_slow = dummp;
+
+    while (p_fast != nullptr && p_fast->next != nullptr) {
+        p_fast = p_fast->next->next;
+        p_slow = p_slow->next;
+    }
+    return p_slow;
+}
