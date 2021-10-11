97d3a3f0b75f896c9075f398ac1954baa39d9669
Jiawei <40544598+Jiawch@users.noreply.github.com>
2021-10-11 11:28:05 +0800

    Update quickSort3Way.h

--- Algorithms/sort/quickSort3Way.h
+++ Algorithms/sort/quickSort3Way.h
@@ -42,3 +42,33 @@ void QuickSort3Way<T>::sort(vector<T> &a, int low, int high){
 
 #endif
 
+
+/*! Author: Jiawch
+ *! Date: 2021-10-11
+ * 三向切分快速排序
+ * ref: https://www.jianshu.com/p/d70aeccaee19
+ */
+
+void quickSort3Way(vector<int> &a) {
+    int low = 0;
+    int high = a.size() - 1;
+    _quickSort3Way(a, low, high);
+}
+
+void _quickSort3Way(vector<int> &a, int low, int high) {
+    if (low >= high)    return;
+
+    int lt = low,           // 指向小于比较元素的向右一位，即等于比较元素的第一位 
+        mid = low + 1,      // 指向当前正在比较的元素
+        gt = high;          // 指向大于比较元素的向左一位，最终指向等于比较元素的最后一位
+
+    int key = a[low];
+    while (mid <= gt) {
+        if (a[mid] < key)   swap(a[mid++], a[lt++]);
+        else if (a[mid] > key)  swap(a[mid], a[gt--]);
+        else    mid++;
+    }
+
+    _quickSort3Way(a, low, lt-1);
+    _quickSort3Way(a, gt+1, high);
+}
