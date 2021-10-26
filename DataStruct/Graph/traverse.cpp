78f12f5c68dad0672f1035b2b8296f103033e582
Jiawei <40544598+Jiawch@users.noreply.github.com>
2021-10-26 16:21:03 +0800

    Update traverse.cpp

--- DataStruct/Graph/traverse.cpp
+++ DataStruct/Graph/traverse.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include <queue>
 #define MAX 100
+#define MAXVEX 100
 
 using namespace std;
 
 
+/* 邻接矩阵数据结构
+ * 包含：
+ *  1. 顶点数
+ *  2. 边数
+ *  3. 顶点表，一维数组表示
+ *  4. 边表，二维数组表示
+ */
+
+struct MGraph {
+    char vexs[MAXVEX];          // 顶点表
+    int arc[MAXVEX][MAXVEX];    // 邻接矩阵，可看作边表
+    int numVertexes;            // 顶点数
+    int numEdges;               // 边数
+};
+
+
 /* 
  * !Date: 2021-10-26
- * 深度优先遍历
+ * （邻接矩阵的）深度优先遍历
  * 思想：回溯
  */
 
@@ -34,7 +51,7 @@ void DFSTraverse (MGraph G) {
 
 /*
  * !Date: 2021-10-26
- * 广度优先遍历
+ * （邻接矩阵的）广度优先遍历
  * 思想：分支限界
  */
 
@@ -65,3 +82,34 @@ void BFSTraverse (MGraph G) {
         }
     }
 }
+
+
+
+
+/* 邻接表数据结构
+ */
+
+struct EdgeNode {       // 边表节点
+    int adjvex;         // 邻接点域，存储该顶点对应下标
+    int weight;         // 权重域
+    EdgeNode *next;     // 键域，指向下一个邻接点
+};
+
+struct VertexNode {         // 顶点表节点
+    int data;               // 顶点域
+    EdgeNode *firstedge;    // 边表头指针
+};
+
+struct GraphList {
+    VertexNode adjList[MAXVEX];
+    int numVertexes;
+    int numEdges;
+};
+
+
+
+
+
+
+
+
