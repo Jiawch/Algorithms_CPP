4967e49036d3169b21ff7e65fcd0dcbfc6336a85
Jiawei <40544598+Jiawch@users.noreply.github.com>
2021-10-27 22:22:55 +0800

    Update heapSort.h

--- Algorithms/sort/heapSort.h
+++ Algorithms/sort/heapSort.h
@@ -61,3 +61,30 @@ void HeapSort<T>::sink(vector<T> &a, int k, int n){
  *  1. 构造大顶堆
  *  2. 将堆顶元素与最后一个元素交换
  */
+
+
+void heapSort(vector<int> &a) {
+    int n = a.size();
+    n = n - 1;      // 让n指向数组的最后一个元素
+
+    // 构造大顶堆，从第一个有孩子的父节点一直往上走
+    for (int i = (n-1) / 2; i >= 0 ; i--)
+        sink(a, i, n);
+
+    // 不断将堆顶元素与最后一个元素交换，并让堆的大小减1
+    for (int i = n; i >= 0; i--) {
+        swap(a[0], a[n--]);
+        sink(a, 0, n);
+    }
+}
+
+// 将第k个元素下沉，构造以k为（完全二叉树）根节点的大顶堆
+void sink(vector<int> &a, int k, int n) {
+    while (2 * k + 1 <= n) {      // k至少还有一个孩子
+        int i = 2 * k + 1;
+        if (i+1<=n && a[i] < a[i+1]) i++;   // 取出孩子结点中大的那个
+        if (a[k] > a[i]) break;             // 如果孩子结点中大的那个也比a[k]小，则下沉结束，退出循环
+        swap(a[k], a[i]);
+        k = i;
+    }
+}
