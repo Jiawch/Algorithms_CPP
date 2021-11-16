cb3582b2ab51ace7bbef21f3007497b3231666a1
Jiawei <40544598+Jiawch@users.noreply.github.com>
2021-11-16 11:07:08 +0800

    Update insert.cpp

--- DataStruct/Tree/SearchTree/insert.cpp
+++ DataStruct/Tree/SearchTree/insert.cpp
@@ -15,6 +15,17 @@ struct TreeNode {
     TreeNode (int val): val(val), left(nullptr), right(nullptr) {}
 };
 
+/* 为什么要返回根节点，如果没有返回根节点会如何
+ * 正常的算法流程应该是：
+ *  如果和更节点一样 -> 不插入
+ *  如果小于根节点 -> 在其左子树插入
+ *  如果大于根节点 -> 在其右子树插入
+ *  递归结束条件则是，当节点为空，new TreeNode(value)。
+ *  但这样有一个问题，如何确定new节点是左孩子还是右孩子。
+ *  所以，需要把左/右孩子信息记下来，而递归中访问之前信息有二法：
+ *      1. 引用变量
+ *      2. 递归函数返回值，此法只能访问到上一级
+ */
 
 /* 二叉查找树的插入（递归）
  * 如果当前节点为空，返回new节点
@@ -22,14 +33,26 @@ struct TreeNode {
  *  1. 当前节点的值>插入值，那么在左子树进行插入
  *  2. 当前节点的值<插入值，那么在右子树进行插入
  * 返回根节点
+ * highlight: 在查找树root插入value，并返回根节点
  */
-TreeNode* insert(TreeNode* root, int val) {
-    if (!root) return new TreeNode(val);
-    if (root->val > val)
-        root->left = insert(root->val, val);
-    else
-        root->right = insert(root->right, val);
+TreeNode* insert(TreeNode* root, int value)
+{
+    if (root == nullptr)
+    {
+        return new TreeNode(value);
+    }
+
+    if (root->value < value)
+    {
+        root->right = insert(root->right, value);
+    }
+    else if (root->value > value)
+    {
+        root->left = insert(root->left, right);
+    }
+
     return root;
+
 }
 
 
@@ -39,24 +62,44 @@ TreeNode* insert(TreeNode* root, int val) {
  *  1. 当前节点的值>插入值，左节点存在则往左走，否则插入新节点
  *  2. 当前节点的值<插入值，右节点存在则往右走，否则插入新节点
  */
-TreeNode* insertNoRecursion(TreeNode* root, int val) {
-    if (!root) return new TreeNode(val);
-    TreeNode* p = root;
-    while (p) {
-        if (p->val > val) {
-            if (p->left) p = p->left;
-            else {
-                p->left = new TreeNode(val);
+TreeNode* insert(TreeNode* root, int value)
+{
+    if (root == nullptr)
+    {
+        return root;
+    }
+
+    TreeNode *p = root;
+    while (p != nullptr)
+    {
+        if (p->value < target)
+        {
+            if (p->right != nullptr)
+            {
+                p = p->right;
+            }
+            else
+            {
+                p->right = new TreeNode(value);
                 break;
             }
         }
-        else {
-            if (p->right) p = p->right;
-            else {
-                p->right = new TreeNode(val);
+        else if (p->value > target)
+        {
+            if (p->left != nullptr)
+            {
+                p = p->left;
+            }
+            else
+            {
+                p->left = new TreeNode(value);
                 break;
             }
+        }
+        else{
+            break;
         }
     }
-    return p;
+
+    return root;
 }
