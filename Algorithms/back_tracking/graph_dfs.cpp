d3035008b40559495afdf3653b6a791e1dec2118
Jiawei <40544598+Jiawch@users.noreply.github.com>
2021-12-04 16:17:27 +0800

    Update graph_dfs.cpp

--- Algorithms/back_tracking/graph_dfs.cpp
+++ Algorithms/back_tracking/graph_dfs.cpp
@@ -16,7 +16,6 @@ DFS(数组/图，参数):
  *  要求每一条路径，则需要两个变量，
  *  保存路径度变量array，保存全部路径度变量matrix
  */
-
 vector<bool> visited(MAX);  // 默认为0，即为false
 void permutation(vector<int> nums, vector<int>& array, vector<vector<int>>& matrix) {
     // 递归结束条件
@@ -36,6 +35,25 @@ void permutation(vector<int> nums, vector<int>& array, vector<vector<int>>& matr
             // 撤销
             array.pop_back();
             visited[i] = false;
+        }
+    }
+}
+
+
+// 图DFS
+struct Graph {
+    vector<int> vexs;   // 顶点
+    vector<vector<int>> adj_matrix; // 边，邻接矩阵表示
+};
+
+vector<bool> visited(MAX);  // 默认为0，即为false
+void DFS(Graph g, int i) {
+    visit(g.vexs[i]);
+    visited[i] = true;
+
+    for (int j = 0; j < g.vexs.size(); j++) {
+        if (g.adj_matrix[i][j] == 1 && visited[j] == false) {
+            DFS(g, j);
         }
     }
 }
